11de9ac7ff8e412ccac19140c76c2803b010a3f0
sebastianvier <seb.vier@gmail.com>
2022-04-28 11:15:35 +0200

    I have been able to add a first entry to my db; Now I am trying to insert everything. For this I am going to create a function

--- table_creation.sql
+++ table_creation.sql
@@ -3,23 +3,17 @@ CREATE DATABASE formula_1;
 
 USE formula_1;
 
+## 1) First table Drivers
+## This table does not have foreign keys
 
 DROP TABLE drivers;
-
 CREATE TABLE drivers (
 	DriverId INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     FirstName varchar(50) NOT NULL,
     LastName varchar(50) NOT NULL,
-    BirthDate DATE,
+    BirthDate date,
 	Nationality varchar(50),
     Gender varchar(50)
 );
 
 SELECT * FROM DRIVERS;
-
-INSERT INTO drivers (FirstName,LastName,BirthDate, Nationality, Gender) 
-VALUES ("a",
-		"a",
-        STR_TO_DATE('1-01-2012', '%d-%m-%Y'),
-        "Hello",
-        "GoodBye");
